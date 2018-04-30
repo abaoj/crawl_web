246e06ccf1f88e69ae3d11dd875bf40605fa2daf
abao123 <liu.baobaoyx@163.com>
2018-04-30 13:42:51 +0800

    taobao

--- 爬取淘宝商品信息/taobao2.py
+++ 爬取淘宝商品信息/taobao2.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import re
+import json
 import urllib.request
 
 def spider_taobao(url):
@@ -49,12 +50,21 @@ def spider_taobao(url):
         spimgs = re.findall(s_img, res)
 
         #特点说明
-        # attributes_html = re.compile('<div id="attributes" class="attributes">(.+?)<div>')
-        # attributes = re.compile('<div id="attributes" class="attributes">.*?</div>', res)
-        attributes =re.findall('<div id="attributes" class="attributes">(.+?)</div>', res, re.S)
-        print(attributes)
+        attributes =re.findall('<ul class="attributes-list">(.+?)</ul>', res, re.S)[0]
+        attributes = re.findall('<li title="(.*?)">(.*?)</li>', attributes, re.S)
+        attrstr = ""
+        for i in range(len(attributes)):
+            attrstr = attrstr + attributes[i][1]+"\n"
 
 
+        ####详情图片
+        # print(res)
+        detail = re.findall(r'var g_config =.*g_config.tadInfo =', res,re.S)[0]
+        detail = re.findall(r'{.*}', detail, re.S)[0]
+        detail = detail.replace(' ', '').replace("\n", "")
+        detail = re.findall('auctionImages:\[(.*?)\]},seller', detail, re.S)[0]
+        # detail = json.loads(detail)
+        # print(detail)
 
         # 45-53行为抓取评论数据，该数据也是动态加载的
         # comment_url = "https://rate.tmall.com/list_detail_rate.htm?itemId={}&sellerId=880734502&currentPage=1".format(
@@ -69,13 +79,14 @@ def spider_taobao(url):
     except Exception as e:
         print('数据抽取失败!!!', e)
 
-
     print('商品名:', title)
     print('划线价格:', line_price)
     print('真实价格:', real_price)
     print('商品链接:', url)
     print('主图',mainimgs)
     print('颜色图',spimgs)
+    print('特性', attrstr)
+    print('详情图片', detail)
     # print('部分评论内容:', comment)
 
 
