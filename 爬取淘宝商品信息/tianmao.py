da270f7a5b37b3044ab74cfb196f5ea0b3134953
abao123 <abao123>
2018-05-01 10:21:57 +0800

    天猫

--- 爬取淘宝商品信息/tianmao.py
+++ 爬取淘宝商品信息/tianmao.py
@@ -1,93 +1,93 @@
-#conding=utf-8
-import re
-import urllib.request
-
-def spider_tianmao(url):
-    headers = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'zh-CN,zh;q=0.3',
-        'Referer': 'https://detail.tmall.com/item.htm',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-        'Connection': 'keep-alive',
-    }
-    goods_id = re.findall('id=(\d+)', url)[0]
-    try:
-        req = urllib.request.Request(url=url, headers=headers)
-        res = urllib.request.urlopen(req).read().decode('gbk', 'ignore')
-    except Exception as e:
-        print( '无法打开网页:', e.reason)
-
-    try:
-        title = re.findall('<input type="hidden" name="title" value="(.*?)"', res)
-        title = title[0] if title else None
-        tm_prices = re.findall('<span class="tm-price">(.*?)</span>', res,re.S)
-
-
-        # 30-42行为抓取淘宝商品真实价格，该数据是动态加载的
-        # purl = "https://detail.tmall.com/item.htm?spm={}&id={}&skuId={}&standard=1&user_id={}&cat_id={}&is_b=1&rn={}".format(goods_id)
-        # price_req = urllib.request.Request(url=purl, headers=headers)
-        # price_res = urllib.request.urlopen(price_req).read()
-        # price_res = price_res.decode('utf-8')  # python3
-        # ss=re.findall('"price":"(.*?)"', price_res)
-        # data = list(set(ss))
-        # # data列表中的价格可能是定值与区间的组合，也可能只是定值，而且不一定有序
-        # real_price = ""
-        # for t in data:
-        #     if '-' in t:
-        #         real_price = t
-        #         break
-        # if not real_price:
-        #     real_price = sorted(map(float, data))[0]
-
-        #主图获取
-        m_img = re.compile('<a href="#"><img src=\"(.+?)\"')
-        mainimgs = re.findall(m_img, res)
-
-        # 颜色图
-        # s_img = re.compile('<a href="javascript:;" style="background:url\\((.+?)\\) center no-repeat;"')
-        # spimgs = re.findall(s_img, res)
-
-        #特点说明
-        # attributes_html = re.compile('<div id="attributes" class="attributes">(.+?)<div>')
-        # attributes = re.compile('<div id="attributes" class="attributes">.*?</div>', res)
-        attributes =re.findall('<ul id="J_AttrUL">(.+?)</ul>', res, re.S)[0]
-        # attributes1=urllib.request.urlopen(attributes).read().decode('gbk', 'ignore')
-        attributes1 =re.findall('<li (.+?)</li>',attributes)
-         len(attributes1)
-        if i < a
-            attributes2[i] = re.findall('<li (.+?)</li>', attributes1[i])
-            print(attributes2[i])
-
-
-
-
-        # 45-53行为抓取评论数据，该数据也是动态加载的
-        # comment_url = "https://rate.tmall.com/list_detail_rate.htm?itemId={}&sellerId=880734502&currentPage=1".format(
-        #     goods_id)
-        # comment_data = urllib.request.urlopen(comment_url).read().decode("GBK", "ignore")
-        # temp_data = re.findall('("commentTime":.*?),"days"', comment_data)
-        # temp_data = temp_data if temp_data else re.findall('("rateContent":.*?),"reply"', comment_data)
-        # comment = ""
-        # for data in temp_data:
-        #     comment += data.encode('utf-8')
-        # comment = comment if comment else "暂无评论"
-    except Exception as e:
-        print('数据抽取失败!!!', e)
-
-    print(type(req))
-    print(type(res))
-    print(tm_prices)
-    print('商品名:', title)
-    # print('划线价格:', tm_price)
-    # print('真实价格:', real_price)
-    print('商品链接:', url)
-    print('主图',mainimgs)
-    # print('颜色图',spimgs)
-    # print('部分评论内容:', comment)
-
-
-if __name__ == '__main__':
-    url = 'https://detail.tmall.com/item.htm?spm=a220m.1000858.1000725.6.2a5a7040MCDYOi&id=529408043334&skuId=3154155972259&standard=1&user_id=1714128138&cat_id=50918010&is_b=1&rn=3fec71e43d00bacc5a47834d4494d206'
-    # url = input("请输入商品链接: ")
-    spider_tianmao(url)
-
+# #conding=utf-8
+# import re
+# import urllib.request
+#
+# def spider_tianmao(url):
+#     headers = {
+#         'Accept': 'application/json, text/plain, */*',
+#         'Accept-Language': 'zh-CN,zh;q=0.3',
+#         'Referer': 'https://detail.tmall.com/item.htm',
+#         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+#         'Connection': 'keep-alive',
+#     }
+#     goods_id = re.findall('id=(\d+)', url)[0]
+#     try:
+#         req = urllib.request.Request(url=url, headers=headers)
+#         res = urllib.request.urlopen(req).read().decode('gbk', 'ignore')
+#     except Exception as e:
+#         print( '无法打开网页:', e.reason)
+#
+#     try:
+#         title = re.findall('<input type="hidden" name="title" value="(.*?)"', res)
+#         title = title[0] if title else None
+#         tm_prices = re.findall('<span class="tm-price">(.*?)</span>', res,re.S)
+#
+#
+#         # 30-42行为抓取淘宝商品真实价格，该数据是动态加载的
+#         # purl = "https://detail.tmall.com/item.htm?spm={}&id={}&skuId={}&standard=1&user_id={}&cat_id={}&is_b=1&rn={}".format(goods_id)
+#         # price_req = urllib.request.Request(url=purl, headers=headers)
+#         # price_res = urllib.request.urlopen(price_req).read()
+#         # price_res = price_res.decode('utf-8')  # python3
+#         # ss=re.findall('"price":"(.*?)"', price_res)
+#         # data = list(set(ss))
+#         # # data列表中的价格可能是定值与区间的组合，也可能只是定值，而且不一定有序
+#         # real_price = ""
+#         # for t in data:
+#         #     if '-' in t:
+#         #         real_price = t
+#         #         break
+#         # if not real_price:
+#         #     real_price = sorted(map(float, data))[0]
+#
+#         #主图获取
+#         m_img = re.compile('<a href="#"><img src=\"(.+?)\"')
+#         mainimgs = re.findall(m_img, res)
+#
+#         # 颜色图
+#         # s_img = re.compile('<a href="javascript:;" style="background:url\\((.+?)\\) center no-repeat;"')
+#         # spimgs = re.findall(s_img, res)
+#
+#         #特点说明
+#         # attributes_html = re.compile('<div id="attributes" class="attributes">(.+?)<div>')
+#         # attributes = re.compile('<div id="attributes" class="attributes">.*?</div>', res)
+#         attributes =re.findall('<ul id="J_AttrUL">(.+?)</ul>', res, re.S)[0]
+#         # attributes1=urllib.request.urlopen(attributes).read().decode('gbk', 'ignore')
+#         attributes1 =re.findall('<li (.+?)</li>',attributes)
+#          len(attributes1)
+#         if i < a
+#             attributes2[i] = re.findall('<li (.+?)</li>', attributes1[i])
+#             print(attributes2[i])
+#
+#
+#
+#
+#         # 45-53行为抓取评论数据，该数据也是动态加载的
+#         # comment_url = "https://rate.tmall.com/list_detail_rate.htm?itemId={}&sellerId=880734502&currentPage=1".format(
+#         #     goods_id)
+#         # comment_data = urllib.request.urlopen(comment_url).read().decode("GBK", "ignore")
+#         # temp_data = re.findall('("commentTime":.*?),"days"', comment_data)
+#         # temp_data = temp_data if temp_data else re.findall('("rateContent":.*?),"reply"', comment_data)
+#         # comment = ""
+#         # for data in temp_data:
+#         #     comment += data.encode('utf-8')
+#         # comment = comment if comment else "暂无评论"
+#     except Exception as e:
+#         print('数据抽取失败!!!', e)
+#
+#     # print(type(req))
+#     # print(type(res))
+#     # print(tm_prices)
+#     # print('商品名:', title)
+#     # # print('划线价格:', tm_price)
+#     # # print('真实价格:', real_price)
+#     # print('商品链接:', url)
+#     # print('主图',mainimgs)
+#     # print('颜色图',spimgs)
+#     # print('部分评论内容:', comment)
+#
+#
+# if __name__ == '__main__':
+#     url = 'https://detail.tmall.com/item.htm?spm=a220m.1000858.1000725.6.2a5a7040MCDYOi&id=529408043334&skuId=3154155972259&standard=1&user_id=1714128138&cat_id=50918010&is_b=1&rn=3fec71e43d00bacc5a47834d4494d206'
+#     # url = input("请输入商品链接: ")
+#     spider_tianmao(url)
+#
